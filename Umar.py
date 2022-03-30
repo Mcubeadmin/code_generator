7e5fbc0429c8889a3d69232755ac9e75735b73df
Mcube <mcubemujahid@gmail.com>
2022-03-30 21:09:25 +0530

    load type generation done

--- Umar.py
+++ Umar.py
@@ -1,1 +1,13 @@
-print("hello, world")
+loadNum = int(input("Enter the No of Load Cases: "))
+Lists = []
+
+print("Enter the Load type and load title seperated by (,)")
+for i in range(loadNum):
+    data = input("Enter the data: ")
+    splitdata = data.split(",")
+    Lists.append(splitdata)
+
+print(Lists)
+
+for i in range(loadNum):
+    print("LOAD " + str(i+1) + " LOADTYPE " + Lists[i][0] + " TITLE " + Lists[i][1])
