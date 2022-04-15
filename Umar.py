7558c49633365079a95d7040b3eba90ae5b9f85e
Mcube <mcubemujahid@gmail.com>
2022-04-15 06:25:23 +0530

    counting problem solved

--- Umar.py
+++ Umar.py
@@ -1,6 +1,5 @@
 loadNum = int(input("Enter the No of Load Cases: "))
 Lists = []
-count = []
 countList = []
 
 #load type and title input and splitting
@@ -8,28 +7,21 @@ print("Enter the Load type and load title seperated by (,)")
 for i in range(loadNum):
     data = input("Enter the data: ")
     splitdata = data.split(",")
-    print(data)
-    print(splitdata)
+    count = []
     if bool(countList) == False:
-        print("first")
-        count.clear()
         count.append(splitdata[0])
         count.append(1)
+        countList.append(count)
     elif lastinput == splitdata[0]:
-        print("second")
         for j in range(len(countList)):
             if countList[j][0] == lastinput:
                 countList[j][1] = int(countList[j][1]) + 1
                 break
     else:
-        print("third")
-        count.clear()
         count.append(splitdata[0])
         count.append(1)
-    print(count)
+        countList.append(count)
     lastinput = splitdata[0]
-    print(lastinput)
-    countList.append(count)
     Lists.append(splitdata)
 
 print(countList)
@@ -40,8 +32,7 @@ print("\n")
 for i in range(loadNum):
     print("LOAD " + str(i+1) + " LOADTYPE " + Lists[i][0] + " TITLE " + Lists[i][1])
 
-"""#equation input
+#equation input
 eq = input("\nEnter the equation to be generated: ")
 eq = eq.split(" ")
 print(eq)
-"""
